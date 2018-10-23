31ae85d686e401df20575c2e949747881109ce19
gunnaraustrheim <gunnar.austrheim@ntnu.no>
2018-10-23 11:08:33 +0200

    Laget nye boxplot for andel browse og abosulutt browse. Laget barplot for LANDKAT og ALTREG for andel browse

--- BrowsGraz/browsgraz_script.R
+++ BrowsGraz/browsgraz_script.R
@@ -1,45 +1,43 @@
 #Script til Herbivore data
+
+getwd()
+
 library(readxl)
-Sau <- read_excel("Fra erling/Kopi av sau 1949-2015 pr kommune m areal (003)_GA.xlsx")
-View(Kopi_av_sau_1949_2015_pr_kommune_m_areal_003_GA)
-
-
-BrowGraz <- read_excel("Browsing/BrowvsGraz2015.xlsx")
-View(BrowGraz)
-
-BrowGraz0110 <- read_excel("Browsing/BrowvsGraz0110.xlsx")
-View(BrowGraz0110)
-
-dat <- read_excel("BrowsGraz/BrowvsGraz0310_final.xlsx")
+dat <- read_excel("BrowsGraz/BrowvsGraz0310_final.xlsx", 
+                  sheet = "Final B and G 0510")
 View(dat)
-
 
 
 #**********************
 #housekeeping####
-Sau$knr2017   <- as.factor(Sau$knr2017)
-Sau$ART  <-as.factor(Sau$ART)
-Sau$fAAR <-as.factor(Sau$AAR)
+#Sau$knr2017   <- as.factor(Sau$knr2017)
+#Sau$ART  <-as.factor(Sau$ART)
+#Sau$fAAR <-as.factor(Sau$AAR)
 
 
-BrowGraz$fAAR <-as.factor(BrowGraz$AAR)
-
-BrowGraz0310$fAAR <-as.factor(BrowGraz0310$AAR)
+dat$fAAR <-as.factor(dat$AAR)
+dat$LANDKAT <-as.factor(dat$LANDKAT)
+levels(dat$LANDKAT)
+dat$ALTREG <-as.factor(dat$ALTREG)
+table(dat$ALTREG)
+table(dat$ALTREG, dat$LANDKAT)
 
 #********************************
 #Data exploration####
 
-table(Sau$fAAR)
-table(Sau$knr2017,Sau$fAAR)
+# Her kan det være lurt å fjerne kommuner (GJØR TIL na) som har hverken B eller G
+
+#table(Sau$fAAR)
+#table(Sau$knr2017,Sau$fAAR)
 
 
-View(Sau[Sau$knr2017=="1503",])
+#View(Sau[Sau$knr2017=="1503",])
 
 
-plot(Sau$TOTBAR)
+#plot(Sau$TOTBAR)
 
 
-table(BrowGraz$fAAR)
+#table(BrowGraz$fAAR)
 #identify(Sau$TOTBAR)
 #View(Sau[617,])
 
@@ -47,7 +45,73 @@ table(BrowGraz$fAAR)
 #******************************
 #Relationships
 
-boxplot(Sau$TOTBAR~Sau$fAAR)
+boxplot(dat$`Andel B`~dat$fAAR,
+        xlab = "Year", ylab = "Proportion browsing")
+
+par(mar=c(5,5,2,2))
+boxplot((dat$`ABS BROW`)/1000000~dat$fAAR,
+        xlab = "Year", ylab = "Absolute MB browsed\n(ton/1000)",
+        las = 2)
+
+
+#******************************
+# Her lager jeg barplot for LANDKAT per år
+
+#******************************
+
+
+landkat_yr <- aggregate(data = dat,
+                        `Andel B` ~ fAAR + LANDKAT,
+                        FUN = mean, na.rm = T)
+std <- function(x) sd(x)/sqrt(length(x))
+landkat_yr_se <- aggregate(data = dat,
+                        `Andel B` ~ fAAR + LANDKAT,
+                        FUN = std)
+landkat_yr$se <- landkat_yr_se$`Andel B`
+
+library(ggplot2)
+
+ggplot(data = landkat_yr, aes(x= fAAR, y = `Andel B`))+
+  geom_bar(stat = "identity")+
+  xlab("Year")+ylab("Mean proportion browsing")+
+  geom_errorbar(aes(ymin = `Andel B` - se, ymax = `Andel B` + se), width = 0.2)+
+  facet_wrap(~LANDKAT, ncol = 4)+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+#******************************
+# Her SKAL jeg lage  barplot for ALTREG per år
+
+#******************************
+
+altreg_yr <- aggregate(data = dat,
+                        `Andel B` ~ fAAR + ALTREG,
+                        FUN = mean, na.rm = T)
+#std <- function(x) sd(x)/sqrt(length(x))
+altreg_yr_se <- aggregate(data = dat,
+                           `Andel B` ~ fAAR + ALTREG,
+                           FUN = std)
+altreg_yr$se <- altreg_yr_se$`Andel B`
+
+library(ggplot2)
+
+ggplot(data = altreg_yr, aes(x= fAAR, y = `Andel B`))+
+  geom_bar(stat = "identity")+
+  xlab("Year")+ylab("Mean proportion browsing")+
+  geom_errorbar(aes(ymin = `Andel B` - se, ymax = `Andel B` + se), width = 0.2)+
+  facet_wrap(~ALTREG, ncol = 3)+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+
+#******************************
+
+
+
+
+
 
 boxplot(BrowGraz$`Andel G TOT`~BrowGraz$fAAR)
 boxplot(BrowGraz$`Andel B TOT`~BrowGraz$fAAR)
