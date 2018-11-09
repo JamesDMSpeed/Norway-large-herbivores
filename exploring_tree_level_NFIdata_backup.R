e5522f642b19c1bdc3689b44ce7628103091b3b9
gunnaraustrheim <gunnar.austrheim@ntnu.no>
2018-11-09 15:16:35 +0100

    calculating relative change in rowan and browsing biomass from cycle 7 to cycle 10

--- exploring_tree_level_NFIdata_backup.R
+++ exploring_tree_level_NFIdata_backup.R
@@ -16,6 +16,12 @@ dat <- read.csv2("T:/vm/alle/Bruker/gunnar/SUSTHERB/NFI/Fra Erling/Tredata 1986-
 # Learning the columns: ####
 #change cloumn name
 colnames(dat)[1] <- "FLATEID"
+
+
+
+
+#*****************
+# Data exploration ####
 # How many 'flater' is there?
 length(unique(dat$FLATEID))
 # 13219
@@ -59,7 +65,7 @@ plot(dat$BRYSTH_DIA[1:1000], dat$TREHOYDE[1:1000])
 plot(dat$BRYSTH_DIA[1:1000], dat$BEREGNET_HOYDE[1:1000])# It's just a column of 1's. 
 
 
-
+# Defining NFI cyckles####
 summary(dat$SESONG)
 # SESONG is year. I could convert this to be cycle 6-10 instead
 #lage en ny kolonne som heter syklus
@@ -83,7 +89,7 @@ summary(dat$AVS_FLATESENT)
 unique(dat$HEL_DELT_FLATE) 
 summary(dat$FLATEDELX) 
 length(unique(dat$FLATEDELX)) #I think this has something to do with the proportion of the plots that was surveyed.
-
+hist((dat$FLATEDELX))
 
 
 
@@ -134,3 +140,153 @@ hist(rowan3$diff)
 summary(rowan3$diff)
 mean(rowan3$diff, na.rm=T)
 sd(rowan3$diff, na.rm=T)
+
+#for osp
+
+summary(dat$SESONG)
+# SESONG is year. I could convert this to be cycle 6-10 instead
+#lage en ny kolonne som heter syklus
+dat$syklus<-0
+dat$syklus[dat$SESONG<=1993]<-6
+dat$syklus[dat$SESONG>=1994]<-7
+dat$syklus[dat$SESONG>=2000]<-8
+dat$syklus[dat$SESONG>=2005]<-9
+dat$syklus[dat$SESONG>=2010]<-10
+dat$syklus[dat$SESONG>=2015]<-11
+table(dat$syklus)
+
+summary(dat$AVS_FLATESENT)
+# trees are between 0 and 89 m from the plot centre. 
+# Not important to keep that data for our analyses.
+
+
+
+
+# I thing not all plots were surveyed completely:
+unique(dat$HEL_DELT_FLATE) 
+summary(dat$FLATEDELX) 
+length(unique(dat$FLATEDELX)) #I think this has something to do with the proportion of the plots that was surveyed.
+
+
+
+
+# subsetting the datafile to make it more managable
+
+dat <- select(dat, 
+              flateID = FLATEID,
+              HEL_DELT_FLATE,
+              year = SESONG,
+              treeID = TREID,
+              species = treart,
+              DBH = BRYSTH_DIA,
+              TreeHeigth = BEREGNET_HOYDE,
+              flatedel = FLATEDEL,
+              flatedelX = FLATEDELX,
+              kommune = KOMNR,
+              fylke = FYLNR,
+              klavetre_provetre = TRETYPE)
+# Mangler bonitet, vegetasjonstype og hogstklasse, men kanskje dette ligegr i 'flatadataene.
+
+
+
+
+
+# DBH growth over one cycle ####
+
+# for osp:
+unique(dat$treart)
+aspen <- dat[dat$treart == "Osp"
+             & dat$syklus == 9 | 
+               dat$treart == "Osp"
+             & dat$syklus == 10, 
+             c(51, 10, 6) # dette er kolonner
+             ]
+names(dat)
+# Dette kan også skrives slik:
+aspen <- dat[dat$treart == "Osp"
+             & dat$syklus == 9 | 
+               dat$treart == "Osp"
+             & dat$syklus == 10, 
+             c( "syklus" , "BRYSTH_DIA", "TREID") # dette er kolonner
+             ]
+
+
+aspen$syklus <- as.factor(aspen$syklus)
+library(reshape2)
+
+aspen2 <- dcast(data = aspen,
+                TREID ~ syklus,
+                value.var = "BRYSTH_DIA",
+                fun.aggregate = mean)
+
+aspen2$diff <- aspen2$`10` - aspen2$`9`
+
+aspen3 <- aspen2[-aspen2$diff <50,]
+plot(aspen3$diff)
+hist(aspen3$diff)
+
+summary(aspen3$diff)
+mean(aspen3$diff, na.rm=T)
+sd(aspen3$diff, na.rm=T)
+
+# DBH growth over one cycle ####
+
+# for selje:
+unique(dat$treart)
+willow <- dat[dat$treart == "Selje"
+             & dat$syklus == 9 | 
+               dat$treart == "Selje"
+             & dat$syklus == 10, c(51, 10, 6)
+             ]
+
+willow$syklus <- as.factor(willow$syklus)
+library(reshape2)
+
+willow2 <- dcast(data = willow,
+                TREID ~ syklus,
+                value.var = "BRYSTH_DIA",
+                fun.aggregate = mean)
+
+willow2$diff <- willow2$`10` - willow2$`9`
+
+willow3 <- willow2[-willow2$diff <50,]
+plot(willow3$diff)
+hist(willow3$diff)
+
+summary(willow3$diff)
+mean(willow3$diff, na.rm=T)
+sd(willow3$diff, na.rm=T)
+
+# Areadatafile ######
+library(readr)
+Areadata <- read_delim("M:/Mine dokumenter/Moose/Beitetrykk_2017/Browsing/NFI/Trerekruttering 1986-2017 60-80 mmdbh.csv", 
+                       ";", escape_double = FALSE, trim_ws = TRUE)
+View(Areadata)
+Areadata2 <- select(Areadata, 
+                    FLATEID, FYLNR, KOMNR, aar, syklus = takst, Rognprha)
+# Selects relavant columns from flatedata
+Areadata3 <- Areadata2[Areadata2$syklus==6 | Areadata2$syklus==10, ]
+# sletter Areadata og Areadata2
+rm(Areadata);rm(Areadata2)
+
+# Get the file with absolute browsing data####
+library(readxl)
+BrowvsGraz <- read_excel("M:/Mine dokumenter/Moose/Beitetrykk_2017/Browsing/Browsing_final 0410/BrowvsGraz2910_final.xlsx")
+View(BrowvsGraz)
+# Selects relavant columns from BrowvsGraz
+BrowvsGraz2 <- select(BrowvsGraz,
+                      KOMNR=knr2017, 
+                      aar=AAR,
+                      Browsing="ABS BROW")
+# sletter BrowsvsGraz
+rm(BrowvsGraz)                 
+# Make a new variable; calculate syklus 10 - syklus 6####
+
+library(reshape2)
+
+Areadata4 <- dcast(data = Areadata3,
+                 FLATEID ~ syklus,
+                 value.var = "Rognprha",
+                 fun.aggregate = mean)
+
+willow2$diff <- willow2$`10` - willow2$`9`            
